9bf76fa1fef552d7e874b641e3747bcf0df7b691
keshana viyanga <101414619+kesh04@users.noreply.github.com>
2025-10-23 12:33:42 +0530

    commit

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery,  } from "@tanstack/react-query";
 import { supabase } from "../supabase-client";
 import { useAuth } from "../context/AuthContext";
 
